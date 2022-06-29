9fb1ca8d31cc76b6f9e3d044db0527779925aad5
Deepak Kumar <56294275+Deepak143341@users.noreply.github.com>
2022-06-29 16:41:16 +0530

    Day5

---
+++ Increasing Triplet Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int x = INT_MAX, y=INT_MAX;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] < x)
+                x = nums[i];
+            else if(nums[i] > x && nums[i] < y)
+                y = nums[i];
+            else{
+                if(nums[i] > x && nums[i] > y)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
